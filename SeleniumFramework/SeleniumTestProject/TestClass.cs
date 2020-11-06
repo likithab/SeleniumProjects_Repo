be73bc5f17db6f5f875a36527708269f9b74e534
likitha b <likithab56@gmail.com>
2020-11-06 20:52:44 +0530

    adding test project

---
+++ SeleniumFramework/SeleniumTestProject/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SeleniumFrameworkBase;
+
+namespace SeleniumTestProject
+{
+    [TestFixture]
+    public class BaseTest : SeleniumBrowserInit
+    {
+        public BaseTest():base("chrome")
+        {
+            driver=InitBrowser();
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            driver.Navigate().GoToUrl("www.google.com");
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
