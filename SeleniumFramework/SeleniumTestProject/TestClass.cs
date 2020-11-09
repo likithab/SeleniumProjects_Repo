498e78064db7e05516ef5f7ad493c2424670f5ca
likitha b <likithab56@gmail.com>
2020-11-09 21:11:30 +0530

    Started adding tests to test https://www.seleniumeasy.com/test/ for selenium test project

--- SeleniumFramework/SeleniumTestProject/TestClass.cs
+++ SeleniumFramework/SeleniumTestProject/TestClass.cs
@@ -1,5 +1,9 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SeleniumFrameworkBase;
+using System;
+using System.Threading;
 
 namespace SeleniumTestProject
 {
@@ -14,8 +18,13 @@ namespace SeleniumTestProject
         [Test]
         public void TestMethod()
         {
-            driver.Navigate().GoToUrl("https://www.google.com/");
-            Assert.AreEqual(driver.Title, "Google");
+            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement popUpElement= wait.Until(ExpectedConditions.ElementIsVisible(By.Id("at-cv-lightbox-close")));
+            popUpElement.Click();
+            driver.FindElement(By.Id("btn_basic_example")).Click();
+            Thread.Sleep(5000);
+
         }
 
         [TearDown]
