2445edfb48857d4cc3fa4fb9a7504942f5cea64a
likitha b <likithab56@gmail.com>
2020-11-30 21:02:57 +0530

    added two tests and separated Base test

---
+++ SeleniumFramework/SeleniumTestProject/BaseTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using SeleniumFrameworkBase;
+using SeleniumTestPageObjects;
+using System.Diagnostics;
+
+namespace SeleniumTestProject
+{
+    [TestFixture]
+    public class BaseTest : SeleniumBrowserInit
+    {
+        SeleniumEasyMainPage mainPage;
+
+        public BaseTest() : base("chrome")
+        {
+            driver = InitBrowser();
+            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/");
+            mainPage = new SeleniumEasyMainPage(driver);
+        }
+
+        [OneTimeSetUp]
+        public void InitialiseTest()
+        {            
+            mainPage.clickOnStartPracticing();
+        }
+
+        [OneTimeTearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+            driver.Quit();
+            Process[] chromeDriverProcesses = Process.GetProcessesByName("chromedriver");
+            foreach (var chromeDriverProcess in chromeDriverProcesses)
+            {
+                chromeDriverProcess.Kill();
+            }
+        }
+    }
+}
--- SeleniumFramework/SeleniumTestProject/SeleniumEasryFormTest.cs
+++ SeleniumFramework/SeleniumTestProject/SeleniumEasryFormTest.cs
@@ -8,49 +8,35 @@ using System.Threading;
 namespace SeleniumTestProject
 {
     [TestFixture]
-    public class BaseTest : SeleniumBrowserInit
+    public class SeleniumEasryFormTest : BaseTest
     {
-        SeleniumEasyMainPage mainPage;
         SimpleFormDemoPage simpleFormPage;
 
-        public BaseTest():base("chrome")
+        [SetUp]
+        public void SimpleFormDemoTestSetup()
         {
-            driver=InitBrowser();
-            driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/");
-        }
-
-        [OneTimeSetUp]
-        public void InitialiseTest()
-        {
-            mainPage = new SeleniumEasyMainPage(driver);
-            mainPage.clickOnStartPracticing();
             simpleFormPage = new SeleniumEasySimpleFromDemo(driver).clickOnSimpleFormDemo();
-        }
-
-        [OneTimeTearDown]
-        public void CloseBrowser()
-        {
-            driver.Close();
-            driver.Quit();
-            Process[] chromeDriverProcesses = Process.GetProcessesByName("chromedriver");
-            foreach (var chromeDriverProcess in chromeDriverProcesses)
-            {
-                chromeDriverProcess.Kill();
-            }
         }
 
         [Test]
         public void SimpleFormPageSimpleInputTest()
         {
             simpleFormPage.singleInputFieldActions("Hello world dot...!");
-            Thread.Sleep(10000);
+            Thread.Sleep(5000);
         }
 
         [Test]
         public void SimpleFormPageMultipleInputTest()
         {
             simpleFormPage.multipleInputFieldAction("1","2");
-            Thread.Sleep(10000);
-        }       
+            Thread.Sleep(5000);
+        } 
+        
+        [TearDown]
+        public void SimpleFormDemoTestTearDown()
+        {
+            driver.Navigate().Back();
+            Thread.Sleep(5000);
+        }
     }
 }
